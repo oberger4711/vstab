454463ec8d5eb2d81fa6846cdb7bc5ce3404202d
Ole Berger <ole.k.berger@t-online.de>
2019-05-03 21:15:52 +0200

    WIP: Use key frames.

--- src/stabilize.cpp
+++ src/stabilize.cpp
@@ -1,5 +1,21 @@
 #include "stabilize.h"
 
+std::vector<cv::DMatch> matchKeyPoints(const cv::Mat& descriptors_reference, const cv::Mat& descriptors_next) {
+  cv::FlannBasedMatcher matcher;
+  std::vector<std::vector<cv::DMatch>> matches_all;
+  matcher.knnMatch(descriptors_reference, descriptors_next, matches_all, 2);
+
+  // Filter matches.
+  // Keep only good matches.
+  std::vector<cv::DMatch> matches_good;
+  for (const auto& neighbours : matches_all) {
+    if (neighbours[0].distance < 0.75 * neighbours[1].distance) {
+      matches_good.push_back(neighbours[0]);
+    }
+  }
+  return matches_good;
+}
+
 void add_motion(std::vector<cv::Mat>& in_out_transforms, const std::vector<cv::Point2f>& centers) {
   for (size_t i = 0; i < in_out_transforms.size(); i++) {
     auto& tf = in_out_transforms[i];
--- src/stabilize.h
+++ src/stabilize.h
@@ -12,107 +12,124 @@
 #include "video.h"
 #include "registration.h"
 
+struct StabilizationParameters {
+  float min_key_frame_overlap = 0.7;
+};
+
+std::vector<cv::DMatch> matchKeyPoints(const cv::Mat& descriptors_reference, const cv::Mat& descriptors_next);
+
+cv::Mat estimateTransform(std::vector<cv::Point2f> key_points_reference, std::vector<cv::Point2f
+
 template<typename T>
 std::vector<cv::Mat> stabilize(Video& frames, const bool debug) {
+  const StabilizationParameters params;
   std::vector<cv::Mat> tfs(frames.size());
   for (auto& m : tfs) {
     m = cv::Mat::eye(3, 3, CV_64FC1);
   }
   cv::Ptr<T> detector = T::create();
-  std::vector<cv::KeyPoint> keypoints_current, keypoints_next;
-  cv::Mat descriptors_current, descriptors_next;
-  detector->detectAndCompute(frames[0], cv::Mat(), keypoints_next, descriptors_next);
+  std::vector<cv::KeyPoint> key_points_key_frame, key_points_current, key_points_next;
+  cv::Mat descriptors_key_frame, descriptors_current, descriptors_next;
+  detector->detectAndCompute(frames[0], cv::Mat(), key_points_next, descriptors_next);
+  key_points_key_frame = key_points_next;
+  descriptors_key_frame = descriptors_next;
 
   std::cout << "0 %" << std::endl;
+  size_t i_key_frame = 0;
+  auto& tf_key_frame = tfs[0];
   for (size_t i = 0; i < frames.size() - 1; i++) {
     // Progress output.
     const size_t percent = static_cast<size_t>((static_cast<float>(i) / frames.size()) * 100);
     std::cout << "\e[1A" << percent << " %   " << std::endl;
 
-    auto& frame_current = frames[i];
+    // Apply detector on next frame.
     auto& frame_next = frames[i + 1];
-
-    // Apply detector.
-    keypoints_current.clear();
-    keypoints_current.swap(keypoints_next);
+    key_points_current.clear();
+    key_points_current.swap(key_points_next);
     descriptors_current = descriptors_next.clone();
-    detector->detectAndCompute(frame_next, cv::Mat(), keypoints_next, descriptors_next);
+    detector->detectAndCompute(frame_next, cv::Mat(), key_points_next, descriptors_next);
 
+    auto& tf_current = tfs[i];
     auto& tf_next = tfs[i + 1];
-    if (!descriptors_current.empty() && !descriptors_next.empty()) {
-      // Find keypoint matches.
-      cv::FlannBasedMatcher matcher;
-      std::vector<std::vector<cv::DMatch>> matches_all;
-      matcher.knnMatch(descriptors_current, descriptors_next, matches_all, 2);
 
-      // Filter matches.
-      // Keep only good matches.
-      std::vector<cv::DMatch> matches_good;
-      for (const auto& neighbours : matches_all) {
-        if (neighbours[0].distance < 0.75 * neighbours[1].distance) {
-          matches_good.push_back(neighbours[0]);
+    bool matched_with_good_overlap = false;
+    cv::Mat tf_next_estimated = cv::Mat::eye(3, 3, CV_64FC1);
+    std::vector<cv::Point2f> good_key_points_key_frame, good_key_points_next;
+    std::vector<int> inlier_mask;
+    // Try to match against key frame first but create new key frame if overlap is too low.
+    for (int j = 0; !matched_with_good_overlap && j < 2; j++) {
+      if (!descriptors_key_frame.empty() && !descriptors_next.empty()) {
+        // Find key point matches.
+        std::vector<cv::DMatch> matches_good = matchKeyPoints(descriptors_key_frame, descriptors_next);
+
+        if (matches_good.empty()) {
+          // Extract corresponding key points.
+          for (const auto& match : matches_good) {
+            good_key_points_key_frame.push_back(key_points_key_frame[match.queryIdx].pt);
+            good_key_points_next.push_back(key_points_next[match.trainIdx].pt);
+          }
+
+          // Estimate transform.
+          inlier_mask.clear();
+          //tf_next_estimated = cv::findHomography(good_key_points_next, good_key_points_key_frame, cv::RANSAC, 3.0, inlier_mask);
+          estimateRigidTransform_extended(good_key_points_next, good_key_points_key_frame, false, SamplingMethod::RANSAC, inlier_mask)
+                  .copyTo(tf_next_estimated(cv::Range(0, 2), cv::Range::all()));
+
+          // New key frame if too few associations.
+          if (j == 0) {
+            const size_t num_inliers = static_cast<size_t>(std::accumulate(inlier_mask.cbegin(), inlier_mask.cend(), 0));
+            const float overlap = static_cast<float>(num_inliers) / key_points_next.size();
+            //const size_t num_inliers = matches_good.size();
+            // TODO: Other possible key frame policies:
+            // - IOU between rectangle key frame and transformed next frame < threshold.
+            // - IOU between set of key points transformed into each other frame and then lying inside the other rectangle.
+            if (i_key_frame != i &&
+                !descriptors_current.empty() &&
+                (matches_good.size() == 0 ||
+                 overlap < params.min_key_frame_overlap)) {
+              // New key frame due to small overlap.
+              std::cout << "New key frame. Overlap was " << overlap << "." << std::endl << std::endl;
+              // Update key frame.
+              i_key_frame = i;
+              tf_key_frame = tf_current;
+              key_points_key_frame = key_points_current;
+              descriptors_key_frame = descriptors_current;
+            }
+            else {
+              matched_with_good_overlap = true;
+            }
+          }
+        }
+        else {
+          // No matches available for estimation.
+          std::cerr << "Warning: No matches to estimate transformation." << std::endl << std::endl;
         }
       }
-      if (!matches_good.empty()) {
-        // Keep only close matches. I. e. closer than the median distance.
-        /*
-        std::vector<float> distances(matches_good.size());
-        for (size_t i = 0; i < matches_good.size(); i++) {
-          const auto& pt_current = keypoints_current[matches_good[i].queryIdx].pt;
-          const auto& pt_next = keypoints_next[matches_good[i].trainIdx].pt;
-          distances[i] = cv::norm(pt_next - pt_current);
-        }
-        std::sort(distances.begin(), distances.end());
-        const float median = distances[distances.size() / 2];
-        for (auto it = matches_good.begin(); it != matches_good.end();) {
-          const auto& pt_current = keypoints_current[it->queryIdx].pt;
-          const auto& pt_next = keypoints_next[it->trainIdx].pt;
-          if (cv::norm(pt_next - pt_current) > median) {
-            it = matches_good.erase(it);
-          }
-          else {
-            ++it;
-          }
-        }
-        */
-
-        // Extract corresponding keypoints.
-        std::vector<cv::Point2f> pts_current, pts_next;
-        for (const auto& match : matches_good) {
-          pts_current.push_back(keypoints_current[match.queryIdx].pt);
-          pts_next.push_back(keypoints_next[match.trainIdx].pt);
-        }
-
-        // Estimate transform.
-        std::vector<int> inlier_mask;
-        //tf_next = cv::findHomography(pts_next, pts_current, cv::RANSAC, 3.0, inlier_mask);
-        estimateRigidTransform_extended(pts_next, pts_current, false, SamplingMethod::RANSAC, inlier_mask)
-                .copyTo(tf_next(cv::Range(0, 2), cv::Range::all()));
-
-        // Debug visualize correspondencies.
-        if (debug) {
-          assert(inlier_mask.size() == pts_current.size());
-          std::array<cv::Scalar, 2> colors = {cv::Scalar(100, 100, 255), cv::Scalar(255, 120, 120)};
-          for (size_t j = 0; j < pts_current.size(); j++) {
-            auto& color = colors[inlier_mask[j]];
-            cv::arrowedLine(frame_current,
-                            static_cast<cv::Point2i>(pts_current[j]),
-                            static_cast<cv::Point2i>(pts_next[j]), color);
-          }
-        }
+      else {
+        // No key points available for estimation.
+        std::cerr << "Warning: No key points in key frame or next frame." << std::endl << std::endl;
       }
     }
-    else {
-      // No keypoints available for estimation.
-      std::cerr << "Warning: No keypoints in current or next frame." << std::endl;
-      std::cout << std::endl;
+
+    // Debug visualize correspondencies.
+    if (debug) {
+      assert(inlier_mask.size() == good_key_points_key_frame.size());
+      std::array<cv::Scalar, 2> colors = {cv::Scalar(100, 100, 255), cv::Scalar(255, 120, 120)};
+      for (size_t j = 0; j < good_key_points_key_frame.size(); j++) {
+        auto& color = colors[inlier_mask[j]];
+        cv::arrowedLine(frame_next,
+            static_cast<cv::Point2i>(good_key_points_key_frame[j]),
+            static_cast<cv::Point2i>(good_key_points_next[j]), color);
+      }
     }
 
     if (tf_next.empty()) {
       std::cerr << "Warning: Empty homography for frame " << i << "." << std::endl;
       std::cout << std::endl;
     }
-    tf_next = tfs[i] * tf_next; // Accumulate transforms.
+    // Accumulate transforms.
+    tf_next = tf_key_frame * tf_next;
+
   }
   std::cout << "\e[1A" << "100 %   " << std::endl;
   return tfs;
